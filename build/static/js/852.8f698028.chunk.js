37290e567a7bee73a7e7a2e6e1642fcf27776036
unknown <jeremie2018mihigo@gmail.com>
2024-04-07 08:06:22 +0200

    build

---
+++ build/static/js/852.8f698028.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfront=self.webpackChunkfront||[]).push([[852],{235852:(n,e,r)=>{r.r(e),r.d(e,{default:()=>s});r(747313);var t=r(346417);const s=function(){return(0,t.jsx)("div",{children:"index"})}}}]);
